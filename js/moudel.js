3ffabea71aa4374cba99d3e9cd5425f150e2b593
wasd3044 <wasd3044@163.com>
2018-01-26 20:16:14 +0800

    2222

--- js/moudel.js
+++ js/moudel.js
@@ -1,7 +1,7 @@
 /**
  * Created by sen on 2018/1/26.
  */
-var app=angular.modules('myApp',[])
+var app=angular.module('myApp',[])
 app.controller('myCtrl',['$scope',function($scope){
     $scope.a=[1,2,3,4,5,6]
 
