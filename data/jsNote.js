acfec73364b2bf6b551e8deb1eb2ebc381e76ab1
wasd3044 <wasd3044@163.com>
2018-04-09 20:43:05 +0800

    aaa

--- data/jsNote.js
+++ data/jsNote.js
@@ -4,5 +4,10 @@ data.jsNoteData=[
     "tittle":"数组去重",
     "content":"function aryUnique(ary){\n this.ary2=this.ary2||[];\nvar data=ary.shift();\nif(!data){\nreturn this.ary2\n}\nary.indexOf(data)==-1&&this.ary2.push(data);\nreturn aryUnique(ary)\n}\n ",
     "flag":""
+  },
+  {
+    "tittle":"数组去重",
+    "content":"function aryUnique(ary){\n this.ary2=this.ary2||[];\nvar data=ary.shift();\nif(!data){\nreturn this.ary2\n}\nary.indexOf(data)==-1&&this.ary2.push(data);\nreturn aryUnique(ary)\n}\n ",
+    "flag":""
   }
 ]
