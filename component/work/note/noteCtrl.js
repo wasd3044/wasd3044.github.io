b12dc38c300ba0adea94a950975ec2276c5319fe
wx-6130 <wx-gaoxw@dtdream.com>
2020-09-21 17:57:20 +0800

    笔记

--- component/work/note/noteCtrl.js
+++ component/work/note/noteCtrl.js
@@ -1,10 +1,25 @@
-define(function (angular, main) {
+define(function () {
     'use strict';
     blogApp.registerController('noteCtrl', function ($scope, $stateParams, $state, $$http) {
         var vm = this;
         $$http.get('note').then(function (value) {
-            vm.notes = value.note;
+            vm.notes = init(value.note);
         });
+        function init(notes) {
+            angular.forEach(notes,function (obj) {
+                var line = 0
+                angular.forEach(obj.function, function (item) {
+                    var lines = item.match(/[\n\r]/gi) || [];
+                    if(line < lines.length) {
+                        line = lines.length;
+                    }
+                });
+                obj.style = {
+                    'height': ((line * 22) + 90) + 'px',
+                };
+            });
+            return notes;
+        }
         vm.search = function () {
             vm.notes = vm.notes.filter(function (item) {
                 return item.lable.indexOf(vm.searchValue) > -1
