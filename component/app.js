c27ed0df680854e65339a958c31bb1aac2b0914e
wx-6130 <wx-gaoxw@dtdream.com>
2020-10-16 16:02:14 +0800

    切换angular10

--- component/app.js
+++ component/app.js
@@ -23,11 +23,11 @@ function mainCtrl($scope, $$http, $state,$stateParams) {
         $state.go(vm.allData[index].mainRouter.state, {author: 'sen'});
         changetype()
     };
-    vm.changeSystem();
     vm.changeSystem = function () {
       history.replaceState("", "", window.location.origin+window.location.pathname+'blog');
       window.location.href = window.location.origin+window.location.pathname;
     };
+    vm.changeSystem();
     vm.isShowTitle = true;
     $scope.changeShowTitle = function (bol) {
         vm.isShowTitle = bol;
