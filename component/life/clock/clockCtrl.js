1b6db70674d96c25f3f372b8c95979daeb9281ca
wx-6130 <wx-gaoxw@dtdream.com>
2020-11-27 12:56:51 +0800

    重写 农历日期

--- component/life/clock/clockCtrl.js
+++ component/life/clock/clockCtrl.js
@@ -1,12 +1,12 @@
 define(function () {
   'use strict';
-  blogApp.registerController('clockCtrl', function ($scope, $interval, calendar, $timeout) {
+  blogApp.registerController('clockCtrl', function ($scope, $interval, $http, $timeout) {
     var vm = this;
-    vm.secondClass = 1;
-    vm.mintClass = 1;
-    vm.dateClass = 1;
-    vm.monthClass = 1;
-    vm.hourClass = 1;
+    vm.second = 1;
+    vm.mint = 1;
+    vm.date = 1;
+    vm.month = 1;
+    vm.hour = 1;
     vm.years = new Date().getFullYear();
     vm.months = [];
     vm.hours = [];
@@ -73,14 +73,26 @@ define(function () {
       vm.date = date;
       vm.month = month;
       vm.hour = hour;
-      var a= calendar.CalConv(getDate.getFullYear(), month, date)
     }
     $interval(function () {
       init()
     }, 1000)
     $scope.$watch('vm.mint', function (newValue) {
-      vm.calendar = calendar.CalConv(vm.years, vm.month, vm.date, vm.hour, vm.mint)
+      // vm.calendar = calendar.CalConv(vm.years, vm.month, vm.date, vm.hour, vm.mint)
     });
+    $scope.$watch('vm.date', function () {
+      var nHours = ['子','丑','寅','卯','辰','巳','午','未','申','酉','戌','亥', '子'];
+      $http.get('https://api.xlongwei.com/service/datetime/convert.json').then(function (data) {
+        var result = data.data;
+        var chinese = result.chinese;
+        vm.calendar.year = chinese.slice(0, chinese.indexOf('年')+1);
+        vm.calendar.month = chinese.slice(chinese.indexOf('年') +1 ,chinese.indexOf('月')+1);
+        vm.calendar.date = chinese.slice(chinese.indexOf('月')+1);
+        vm.calendar.ganzhi = result.ganzhi;
+        vm.calendar.hour = nHours[Math.floor(vm.hour/2)] + '时';
+        vm.calendar.mint = vm.hour%2 ? vm.mint>30 ? '二刻' : '初刻': vm.mint>30 ? '四刻' : '三刻'
+      })
+    })
     //年月日时分秒星期
     // 农历月日时刻节气
     // 节假日
